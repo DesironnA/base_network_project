a7fc00ceb4937560d482cf129086054538e05209
DesironnA <31777731+DesironnA@users.noreply.github.com>
2026-06-28 23:56:12 +0330

    wb3 added

--- crypto_example.py
+++ crypto_example.py
@@ -4,6 +4,8 @@ ACCOUNT = "0xYourAddress"
 KEY = "YOUR_PRIVATE_KEY"
 TARGET = "0xContractAddress"
 
+web3 = Web3(Web3.HTTPProvider(RPC))
+
 # this is a test comment.
 # Please do not use this project for anything illegal.
 # Send me pm on X for any questions. @Qoqnush
